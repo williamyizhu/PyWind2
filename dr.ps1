ffe25e6eedd2a5411137574b33f2b615eb622615
williamyizhu <william.yizhu@live.com>
2017-07-09 22:29:11 +0800

    minor change

--- dr.ps1
+++ dr.ps1
@@ -1,23 +1,26 @@
-ECHO "Daily Routine"
-
 taskkill /f /im WBox.exe
 taskkill /f /im wim.exe
 
 $dd = Get-Date -format yyyyMMdd
 
-$ff1 = "log\contract_active_" + $dd + ".txt"
-$ff2 = "log\wind_dr_" + $dd + ".txt"
-$ff3 = "log\mongodb_dr_" + $dd + ".txt"
+$msg = "Daily Routine " + $dd
+ECHO $msg
 
 ECHO "Run active_contract.py"
-python .\contract_active.py > $ff1
+$ff = "log\contract_active_" + $dd + ".txt"
+python .\contract_active.py > $ff
 
 ECHO "Run wind_dr.py, change wind_mongodb2 to wind_mongodb2i"
-python .\wind_dr.py -m dd um -s trading -tf eod -cc y -d wind_mongodb2 > $ff2
+$ff = "log\wind_dr_" + $dd + ".txt"
+python .\wind_dr.py -m dd um -s trading -tf eod -cc y -d wind_mongodb2 > $ff
 
-ECHO "Run mongodb_dr.py, change wind_mongodb2 to wind_mongodb2i, change ctp_mongodb2 to ctp_mongodb2i"
-python .\mongodb_dr.py -m um -d wind_mongodb2 > $ff3
-python .\mongodb_dr.py -m um -d ctp_mongodb2 > $ff3
+ECHO "Run mongodb_dr.py for db=WindDataCnFutures, change wind_mongodb2 to wind_mongodb2i"
+$ff = "log\mongodb_dr_wind_" + $dd + ".txt"
+python .\mongodb_dr.py -m um -d wind_mongodb2 > $ff
+
+ECHO "Run mongodb_dr.py for db=CtpData, change ctp_mongodb2 to ctp_mongodb2i"
+$ff = "log\mongodb_dr_ctp_" + $dd + ".txt"
+python .\mongodb_dr.py -m um -d ctp_mongodb2 > $ff
 
 taskkill /f /im WBox.exe
 taskkill /f /im wim.exe
