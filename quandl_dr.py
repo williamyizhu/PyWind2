0c9fabfec31ef9726410d6a7421e36b000f1cab4
williamyizhu <william.yizhu@live.com>
2018-12-09 14:05:51 +0800

    Major update

--- quandl_dr.py
+++ quandl_dr.py
@@ -1,61 +1,67 @@
-import os
-import sys
 import argparse
-sys.path.append(os.path.join(os.path.abspath('..'), 'PyShare\\PyShare'))
-import Quandl
-import Mongo
+import os
+import platform
+
+from PyShare.PyUtils import Mongo
+from PyShare.PyUtils import Quandl
+
 
 def func(args):
-#     ------------- parse command line input args -------------
-#     arguments for both wind and quandl
-    rootdir = 'C:\\quandl_data_cn_futures' if args.rootdir is None else args.rootdir[0].lower()
+    # ------------- parse command line input args -------------
+    # arguments for both wind and quandl
+    # rootdir = 'C:\\quandl_data_cn_futures' if args.rootdir is None else args.rootdir[0].lower()
+
+    rootdir = 'C:\\quandl_data_cn_futures' if 'Windows' in platform.system() else '/home/quandl_data_cn_futures'
+
     mode = [] if args.mode is None else [x.lower() for x in args.mode]
     timeframe = ['eod'] if args.timeframe is None else [x.lower() for x in args.timeframe]
     mongodb = '' if args.mongodb is None else args.mongodb[0]
-#     quandl specific argument
-    get_quandl_dataset = False if args.get_quandl_dataset is None else args.get_quandl_dataset[0].lower()=='y'
-    exchange = ['CFFEX','SHFE','DCE','ZCE'] if args.exchange is None else [x.upper() for x in args.exchange]
+    # quandl specific argument
+    get_quandl_dataset = False if args.get_quandl_dataset is None else args.get_quandl_dataset[0].lower() == 'y'
+    exchange = ['CFFEX', 'SHFE', 'DCE', 'ZCE'] if args.exchange is None else [x.upper() for x in args.exchange]
     underlying = [] if args.underlying is None else [x.upper() for x in args.underlying]
-    check_expiration = True if args.check_expiration is None else args.check_expiration[0].lower()=='y'
+    check_expiration = True if args.check_expiration is None else args.check_expiration[0].lower() == 'y'
 
-#     ------------- get data from quandl ------------- 
-#     create Quandl object
+    # ------------- get data from quandl -------------
+    # create Quandl object
     qd = Quandl.Quandl(rootdir, 'quandl_sector.ini', timeframe)
-#     refresh quandl dataset file, and get contract specification
+    # refresh quandl dataset file, and get contract specification
     if get_quandl_dataset:
         qd.get_datasets(exchange)
     contract_spec_update, contract_spec_all = qd.get_contract_spec(exchange, underlying, check_expiration)
 
-#     ------------- get data from quandl -------------
+    # ------------- get data from quandl -------------
     if 'dd' in mode:
         success, failed = qd.get_quandl_data(contract_spec_update)
-        print('\n***********************************************\ndownload results', 
-              [':'.join([a,str(abs(b))]) for a,b in zip(['success','failed'], [len(success), len(failed)])], 
+        print('\n***********************************************\ndownload results',
+              [':'.join([a, str(abs(b))]) for a, b in zip(['success', 'failed'], [len(success), len(failed)])],
               '\n***********************************************\n')
 
-#     ------------- create mongodb handler, upsert into mongodb -------------
+    # ------------- create mongodb handler, upsert into mongodb -------------
     if 'um' in mode:
-        mongo_path = os.path.join(os.path.abspath('..'), 'PyShare', 'config', 'mongodb_connection.ini')
+        mongo_path = os.path.join(os.path.abspath('..'), 'PyConfig', 'config', 'default_mongodb_connection.ini')
         mdb = Mongo.MongoDB(mongo_path)
         mdb.connect(mdb.connection[mongodb])
         qd.mongo_upsert(contract_spec_update, mdb)
 
+
 def main():
     parser = argparse.ArgumentParser(usage='Get Quandl data')
-    parser.add_argument('-dir', '--rootdir', nargs='*', action='store')
+    # parser.add_argument('-dir', '--rootdir', nargs='*', action='store')
     parser.add_argument('-m', '--mode', nargs='*', action='store')
     parser.add_argument('-tf', '--timeframe', nargs='*', action='store')
     parser.add_argument('-d', '--mongodb', nargs='*', action='store')
     parser.add_argument('-g', '--get_quandl_dataset', nargs='*', action='store')
     parser.add_argument('-e', '--exchange', nargs='*', action='store')
     parser.add_argument('-u', '--underlying', nargs='*', action='store')
-    parser.add_argument('-exp', '--check_expiration', nargs='*', action='store')    
+    parser.add_argument('-exp', '--check_expiration', nargs='*', action='store')
     args = parser.parse_args()
-#     print(args)
+    # print(args)
     try:
         func(args)
     except Exception as e:
-        print(__file__, '\n', e)  
+        print(__file__, '\n', e)
+
 
 if __name__ == '__main__':
     main()
