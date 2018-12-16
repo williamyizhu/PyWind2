090f3384cf375093b7d831776a1611ddd9fcb87b
williamyizhu <william.yizhu@live.com>
2018-12-17 00:31:20 +0800

    Remove cleancopy option

--- wind_dr.py
+++ wind_dr.py
@@ -10,7 +10,7 @@ def func(args):
     mode = [] if args.mode is None else [x.lower() for x in args.mode]
     sector = 'trading' if args.sector is None else args.sector[0].lower()
     timeframe = ['eod'] if args.timeframe is None else [x.lower() for x in args.timeframe]
-    cleancopy = False if args.cleancopy is None else args.cleancopy[0].lower() == 'y'
+    # cleancopy = False if args.cleancopy is None else args.cleancopy[0].lower() == 'y'
 
     # mongodb connection ini file
     config_file_prefix = 'default' if args.config_file_prefix is None else args.config_file_prefix[0]
@@ -35,9 +35,9 @@ def func(args):
         mdb = wd.mongo_connect(config_file_prefix, mongodb)
         wd.mongo_upsert(contract_spec_update, mdb)
 
-    # ------------- clean copy dir, rename file name and skip old contracts -------------
-    if cleancopy:
-        wd.cleancopy()
+    # # ------------- clean copy dir, rename file name and skip old contracts -------------
+    # if cleancopy:
+    #     wd.cleancopy()
 
 
 def main():
@@ -45,7 +45,7 @@ def main():
     parser.add_argument('-m', '--mode', nargs='*', action='store', help='-s cfe: only apply to sector=cfe, in wind_section.ini')
     parser.add_argument('-s', '--sector', nargs='*', action='store')
     parser.add_argument('-tf', '--timeframe', nargs='*', action='store')
-    parser.add_argument('-cc', '--cleancopy', nargs='*', action='store', help='-cc y: e.g., rename CZCE.ZC705.csv to CZCE.ZC1705')
+    # parser.add_argument('-cc', '--cleancopy', nargs='*', action='store', help='-cc y: e.g., rename CZCE.ZC705.csv to CZCE.ZC1705')
     parser.add_argument('-id', '--config_file_prefix', nargs='*', action='store', help='config file prefix, e.g., gxjy, default')
     parser.add_argument('-d', '--mongodb', nargs='*', action='store')
     args = parser.parse_args()
@@ -62,10 +62,10 @@ if __name__ == '__main__':
 # cd 'Z:\Documents\workspace\PyWind2'
 
 # daily routine, trading section contains only non-expired contracts, historial section contains all expired contracts
-# python .\wind_dr.py -m dd -s trading -tf eod -cc y
-# python .\wind_dr.py -m dd -s historical -tf eod -cc y
-# python .\wind_dr.py -m dd -s shf-t -tf eod -cc y
+# python .\wind_dr.py -m dd -s trading -tf eod
+# python .\wind_dr.py -m dd -s historical -tf eod
+# python .\wind_dr.py -m dd -s shf-t -tf eod
 
 # python .\wind_dr.py -m dd -s cfe-t -tf eod
-# python .\wind_dr.py -m dd um -s cfe-t -tf eod -cc y -id default -d wind_mongodb2
-# python .\wind_dr.py -m dd um -s trading -tf eod -cc y -id default -d wind_mongodb2
+# python .\wind_dr.py -m dd um -s cfe-t -tf eod -id default -d wind_mongodb2
+# python .\wind_dr.py -m dd um -s trading -tf eod -id default -d wind_mongodb2
